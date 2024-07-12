e3abafd8dcf028ebd80d126e523423ca1e98e880
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2024-07-12 21:29:04 +0600

    success  Done

--- src/pages/CheckoutPage/SuccessPage.tsx
+++ src/pages/CheckoutPage/SuccessPage.tsx
@@ -6,8 +6,26 @@ const SuccessPage = () => {
       <div className="pt-12">
         <br />
       </div>
-      <div className="mt-8">
-        <h2 className="text-xl font-bold">Order Summary</h2>
+      <div className="">
+        {/* <h2 className="text-xl font-bold">Order Summary</h2> */}
+        <div className="card lg:card-side bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/d6J58q1/Email-Messages-for-Order-Confirmation-Page-v3.png"
+              alt="Album"
+            />
+          </figure>
+          <div className="card-body">
+            <div className="text-center">
+              <h2 className="card-title font-extrabold text-green-700">
+                You will get <br /> your product <br /> Within Seven days!
+              </h2>
+            </div>
+          </div>
+          <figure>
+            <img src="https://i.ibb.co/x2c8y0w/success2.jpg" alt="Album" />
+          </figure>
+        </div>
       </div>
     </div>
   );
